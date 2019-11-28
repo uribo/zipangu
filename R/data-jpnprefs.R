5655131e77a1d634c540fbc64d348c52d20c6efc
Shinya Uryu <uryu.shinya@nies.go.jp>
2019-11-28 19:22:53 +0900

    🗾 Add prefecuture dataset
    Original: {jpndistirct}

---
+++ R/data-jpnprefs.R
@@ -0,0 +1,13 @@
+#' Prefectural informations in Japan
+#' @description Prefectures dataset.
+#' @format A tibble with 47 rows 5 variables:
+#' \itemize{
+#'   \item{jis_code: jis code}
+#'   \item{prefecture_kanji: prefecture names}
+#'   \item{prefecture: prefecture names}
+#'   \item{region: region}
+#'   \item{major_island: }
+#' }
+#' @examples
+#' jpnprefs
+"jpnprefs"
