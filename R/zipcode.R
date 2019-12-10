2ec1900407f1adcdbd8196653c9e01db661d1276
Shinya Uryu <uryu.shinya@nies.go.jp>
2019-12-10 19:07:51 +0900

    Add japan zip-code utility functions
    - 日本郵便が提供する郵便番号データ(csv形式)を読み込む
    - 数値・文字列が7桁の郵便番号であるか
    - 郵便番号文字列に含まれるハイフンを除外または追加

---
+++ R/zipcode.R
@@ -0,0 +1,130 @@
+#' Read Japan post's zip-code file
+#' @description
+#' \Sexpr[results=rd, stage=render]{lifecycle::badge("experimental")}
+#' @param path path
+#' @param type Input file type, one of "oogaki", "kogaki", "roman", "jigyosyo"
+#' @return [tibble][tibble::tibble]
+#' @seealso [https://www.post.japanpost.jp/zipcode/dl/readme.html](https://www.post.japanpost.jp/zipcode/dl/readme.html),
+#' [https://www.post.japanpost.jp/zipcode/dl/jigyosyo/readme.html](https://www.post.japanpost.jp/zipcode/dl/jigyosyo/readme.html)
+#' @rdname read_zipcode
+#' @export
+read_zipcode <- function(path, type = c("oogaki", "kogaki", "roman", "jigyosyo")) {
+  rlang::arg_match(type)
+  read_csv_jp <-
+    purrr::partial(utils::read.csv,
+                   fileEncoding = "cp932",
+                   stringsAsFactors = FALSE)
+  address_level <- c("prefecture", "city", "street")
+  col_vars <-
+    list(yomi = c("jis_code", "old_zip_code", "zip_code",
+                  paste0(address_level, "_kana"),
+                  address_level,
+                  "is_street_duplicate",
+                  "is_banchi",
+                  "is_cyoumoku",
+                  "is_zipcode_duplicate",
+                  "status",
+                  "modify_type"
+    ),
+    roman = c("zip_code",
+              address_level,
+              paste0(address_level, "_roman")),
+    jigyosyo = c("jis_code", "name_kana", "name",
+                 address_level,
+                 "street_sub",
+                 "jigyosyo_identifier",
+                 "old_zip_code",
+                 "grouped",
+                 "individual_id",
+                 "multiple_types",
+                 "update_type"))
+  if (type == "oogaki") {
+    df <-
+      read_csv_jp(file = path,
+                  col.names = col_vars$yomi,
+                  colClasses = c(rep("character", 9),
+                                     rep("double", 3),
+                                     rep("double", 3)))
+  } else if (type == "kogaki") {
+    df <-
+      dplyr::mutate_at(read_csv_jp(file = path,
+                                   col.names = col_vars$yomi,
+                                   colClasses = c(rep("character", 9),
+                                                  rep("double", 3),
+                                                  rep("double", 3))),
+                       stringr::str_subset(col_vars$roman, "^is_"),
+                                   as.logical)
+  } else if (type == "roman") {
+    df <-
+      dplyr::mutate_at(read_csv_jp(file = path,
+                                  col.names = col_vars$roman,
+                                  colClasses = rep("character", 7)),
+                       stringr::str_subset(col_vars$roman, "roman$"),
+                       stringr::str_to_title)
+  } else if (type == "jigyosyo") {
+    df <-
+      read_csv_jp(file = path,
+                  col.names = col_vars$jigyosyo,
+                  colClasses = c(rep("character", 10),
+                                 rep("integer", 3)))
+  }
+  tibble::as_tibble(
+    dplyr::mutate_if(
+      dplyr::mutate_if(df,
+                       is.character,
+                       stringi::stri_trans_general,
+                       id = "nfkc"),
+      is.character,
+      stringr::str_trim)
+  )
+}
+
+#' Test zip-code
+#' @description
+#' \Sexpr[results=rd, stage=render]{lifecycle::badge("experimental")}
+#' @param x Zip-code. Number or character. Hyphens may be included,
+#' but the input must contain a 7-character number.
+#' @rdname is_zipcode
+#' @examples
+#' is_zipcode(7000027)
+#' is_zipcode("700-0027")
+#' @export
+is_zipcode <- function(x) {
+  checked <- stringr::str_detect(x, "^([0-9]{3}-[0-9]{4}|[0-9]{3}[0-9]{4})$")
+  if (rlang::is_false(checked))
+    rlang::inform("7\u6841\u306e\u6570\u5024\u3067\u306f\u3042\u308a\u307e\u305b\u3093")
+  checked
+}
+
+#' Insert and remove zip-code connect character
+#' @description
+#' \Sexpr[results=rd, stage=render]{lifecycle::badge("maturing")}
+#' Inserts a hyphen as a delimiter in the given zip-code string.
+#' Or exclude the hyphen.
+#' @inheritParams is_zipcode
+#' @param remove Default is `FALSE`. If `TRUE`, remove the hyphen.
+#' @rdname zipcode_spacer
+#' @examples
+#' zipcode_spacer(7000027)
+#' zipcode_spacer("305-0053")
+#' zipcode_spacer("305-0053", remove = TRUE)
+#' @export
+zipcode_spacer <- function(x, remove = FALSE) {
+  purrr::map_chr(x,
+                 ~ if (rlang::is_true(is_zipcode(.x)))
+                   if (rlang::is_false(remove)) {
+                     if (stringr::str_detect(.x,
+                                             "-",
+                                             negate = FALSE)) {
+                       .x
+                     } else {
+                       paste0(stringr::str_sub(.x, 1, 3),
+                              "-",
+                              stringr::str_sub(.x, 4, 7))
+                     }
+                   } else {
+                     stringr::str_remove_all(.x, "-")
+                   }
+                 else
+                   NA_character_)
+}
---
+++ tests/testthat/test-zipcode.R
@@ -0,0 +1,30 @@
+test_that("zip-code check is works", {
+  expect_true(is_zipcode(7000027))
+  expect_true(is_zipcode("7000027"))
+  expect_true(is_zipcode("305-0053"))
+  expect_message(
+    expect_false(is_zipcode("305-005"))
+  )
+  expect_message(
+    expect_false(is_zipcode("305"))
+  )
+})
+
+test_that("zip-code spacer works", {
+  expect_equal(
+    zipcode_spacer("1000004"),
+    "100-0004"
+  )
+  expect_equal(
+    zipcode_spacer(1000004),
+    "100-0004"
+  )
+  expect_equal(
+    zipcode_spacer("100-0004", remove = TRUE),
+    "1000004"
+  )
+  expect_equal(
+    zipcode_spacer("305-0053", remove = FALSE),
+    "305-0053"
+  )
+})
