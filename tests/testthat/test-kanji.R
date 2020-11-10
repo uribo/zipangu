6ab6f3a6dd45e20fc846d84839c9d13f05f131a7
indenkun <kobamao.jp@gmail.com>
2020-11-10 23:17:24 +0900

    fix a kansuji2arabic test.

--- tests/testthat/test-kanji.R
+++ tests/testthat/test-kanji.R
@@ -36,15 +36,14 @@ test_that("convert kansuji to arabic works", {
   )
   expect_equal(
     kansuji2arabic_str("\u91d1\u4e00\u5104\u4e8c\u5343\u4e09\u767e\u56db\u5341\u4e94\u4e07\u516d\u5343\u4e03\u767e\u516b\u5341\u4e5d\u5186"),
-    "\\u91d1123456789\\u5186"
+    "\u91d1123456789\u5186"
   )
   expect_equal(
     kansuji2arabic_str("\u91d1\u4e00\u5104\u4e8c\u4e09\u56db\u4e94\u4e07\u516d\u4e03\u516b\u4e5d\u5186"),
-    "\\u91d1123456789\\u5186"
-    #' kansuji2arabic_str("\u91d11\u51042345\u4e076789\u5186")
+    "\u91d1123456789\u5186"
   )
   expect_equal(
     kansuji2arabic_str("\u91d11\u51042345\u4e076789\u5186"),
-    "\\u91d1123456789\\u5186"
+    "\u91d1123456789\u5186"
   )
 })
