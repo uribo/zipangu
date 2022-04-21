a6cf6e6e33e8383ebe3456d3e7ebbff30f3e3f17
Doubt-0KB <98461207+Doubt-0KB@users.noreply.github.com>
2022-04-21 23:23:28 +0900

    Add convert_prefecture()
    都道府県名の漢字⇔ローマ字の相互変換

---
+++ R/convert_prefecture.R
@@ -0,0 +1,45 @@
+convert_prefecture_to_kanji <- function(x){
+  x <- enc2utf8(as.character(x)) #Encoding to UTF-8
+  if(any(is_prefecture(x)|x=="全国"))
+    stop("日本語表記が含まれています") #check:is Japanese
+
+  x <- str_to_sentence(x) #Convert to sentence
+  y <- c(zipangu::jpnprefs$prefecture, "All") #Add all
+  z <- c(zipangu::jpnprefs$prefecture_kanji, enc2utf8("全国")) #Add 全国
+
+  for(i in 1:length(x)){
+    if(!any(str_detect(y, x[i]))) stop("存在しない地域が含まれています") #check;is Japan
+    x[i] <- z[str_detect(y, x[i])] #Convert roman to japanese
+  }
+  return(x)
+}
+
+
+
+convert_prefecture_to_roman <- function(x){
+  x <- enc2utf8(as.character(x)) #Encoding to UTF-8
+  if(any(!is_prefecture(x) & x!="全国"))
+    stop("日本語表記以外が含まれています") #check:is Japanese
+
+  x <- harmonize_prefecture_name(x, to="long")
+  y <- c(zipangu::jpnprefs$prefecture, "All")
+  z <- c(zipangu::jpnprefs$prefecture_kanji, enc2utf8("全国"))
+
+  for(i in 1:length(x)){
+    if(!any(str_detect(z, x[i]))) stop("存在しない地域が含まれています") #check;is Japan
+    x[i] <- y[str_detect(z, x[i])] #Convert japanese to roman
+  }
+  return(x)
+}
+
+
+
+convert_prefecture <- function(x, to){
+  if(to=="kanji"){
+    convert_prefecture_to_kanji(x)
+  } else if(to=="roman"){
+    convert_prefecture_to_roman(x)
+  } else {
+    stop("toが正しくありません")
+  }
+}
