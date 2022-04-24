7ecd1ca74162b9812dc6ead2c759c894b806e252
Doubt-0KB <98461207+Doubt-0KB@users.noreply.github.com>
2022-04-24 18:58:10 +0900

    Fix convert_prefecture_from_hirakana
    変換元の都府県の有無に合わせて変換を実施するように修正

--- R/convert_prefecture_from_hirakana.R
+++ R/convert_prefecture_from_hirakana.R
@@ -8,6 +8,7 @@
 #' @examples
 #' convert_prefecture_from_hirakana(c("とうきょうと"))
 #' convert_prefecture_from_hirakana(c("トウキョウト", "キョウト"))
+#' convert_prefecture_from_hirakana(c("トウキョウ", "おおさか"))
 #'
 #' @export
 #'
@@ -31,11 +32,21 @@ convert_prefecture_from_hirakana <- function(x){
                   "滋賀県","京都府","大阪府","兵庫県","奈良県","和歌山県",
                   "鳥取県","島根県","岡山県","広島県","山口県","徳島県",
                   "香川県","愛媛県","高知県","福岡県","佐賀県","長崎県",
-                  "熊本県","大分県","宮崎県","鹿児島県","沖縄県","全国"))
+                  "熊本県","大分県","宮崎県","鹿児島県","沖縄県","全国")) #Prefecture(long)
+  w <- enc2utf8(c("北海道","青森","岩手","宮城","秋田","山形",
+                  "福島","茨城","栃木","群馬","埼玉","千葉",
+                  "東京","神奈川","新潟","富山","石川","福井",
+                  "山梨","長野","岐阜","静岡","愛知","三重",
+                  "滋賀","京都","大阪","兵庫","奈良","和歌山",
+                  "鳥取","島根","岡山","広島","山口","徳島",
+                  "香川","愛媛","高知","福岡","佐賀","長崎",
+                  "熊本","大分","宮崎","鹿児島","沖縄","全国")) #Prefecture(short)
 
   for(i in 1:length(x)){
     if(!any(str_detect(y, x[i]))) stop("存在しない地域が含まれています") #check;is Japan
-    x[i] <- z[str_detect(y, x[i])] #Convert romen to japanese
+    x[i] <- ifelse(any(str_detect(x, y)),
+                   z[str_detect(y, x[i])], #Convert romen to japanese (long)
+                   w[str_detect(y, x[i])]) #Convert romen to japanese (short)
   }
 
   return(x)
